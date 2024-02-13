a10e9674ebed07ff45a2be1228ce27b0832fefc7
Andrew Tate <tate.andrewjames@gmail.com>
2024-02-12 20:52:18 -0500

    add styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      "vs-dark": "#1e1e1e",
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
